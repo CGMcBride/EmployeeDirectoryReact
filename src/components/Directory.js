86ef469a7c6b94bc4fde757b90f3b903584caf7e
CGMcBride <connermcbride@ymail.com>
2020-08-22 13:29:46 -0500

    added search and directory to js

---
+++ src/components/Directory.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Directory extends Component {
+    render() {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                First Name
+                            </th>
+                            <th>
+                                Last Name
+                            </th>
+                            <th>
+                                Email
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        )
+    }
+}
